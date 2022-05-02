2814bbb9c732706af9c633cd7d4fcf12f9ce90b3
Femi Oyeniyi <65442229+Dafirm@users.noreply.github.com>
2022-05-03 01:36:33 +0300

    AJaX
    simplified callback functions

--- Ajax/async.js
+++ Ajax/async.js
@@ -7,7 +7,7 @@ Ajax means Asynchronous JavaScript And XML
 
 window.onload = function(){
 
-  var http = new XMLHttpRequest();//help us make http request and get data back from a server
+  /*var http = new XMLHttpRequest();//help us make http request and get data back from a server
 
   http.onreadystatechange = function(){
     //console.log(http);
@@ -25,4 +25,62 @@ $.get("tweets.json", function(data){
 })
 console.log("test");
 
+
+//using callbacks functions for synchronous
+function callback(val){
+  console.log(val);
+}
+
+var fruits = ["banana", "apple", "pear"];
+fruits.forEach(callback);
+console.log("done");
+
+
+//using callbacks functions for Asynchronous
+
+$.get("data/tweets.json", function(data){
+  console.log(data);
+
+});
+console.log("test");
+*/
+function HandleError(jqXHR, textStatus, error){
+  console.log(error);
+}
+
+$.ajax({
+  type : "GET",
+  url: "data/tweets.json",
+  success : cbTweets,
+  error: HandleError
+  });
+
+  function cdTweets(data){
+    console.log(data);
+
+
+    $.ajax({
+      type : "GET",
+      url: "data/friendss.json",
+      success : cbFriends,
+      error: HandleError
+    )};
+
+    }
+
+  function cdFriends(data){
+    console.log(data)
+
+        $.ajax({
+          type : "GET",
+          url: "data/videos.json",
+          success : function(data){
+          console.log(data);
+
+        },
+
+        error: HandleError
+
+    });
+  }
 };
